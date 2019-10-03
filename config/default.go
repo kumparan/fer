d3ff1c945f0ff89a46c1c640394176d9d51bc406
bambang deny p <bambang.prayitno@kumparan.com>
2019-10-03 16:05:20 +0700

    feature: add all need Contributing to Backend Projects

--- config/default.go
+++ config/default.go
@@ -2,5 +2,15 @@ package config
 
 const (
 	// GoVersion :nodoc:
-	GoVersion = "1.12.7"
+	GoVersion = "1.12.4"
+	// ChangeLogInstallerURL :nodoc:
+	ChangeLogInstallerURL = "github.com/git-chglog/git-chglog/cmd/git-chglog"
+	// ProtobufInstallerURL :nodoc:
+	ProtobufInstallerURL = "github.com/golang/protobuf/protoc-gen-go"
+	// MockgenInstallerURL :nodoc:
+	MockgenInstallerURL = "github.com/golang/mock/mockgen"
+	// RichgoInstallerURL :nodoc:
+	RichgoInstallerURL = "github.com/kyoh86/richgo"
+	// GolintInstallerURL :nodoc:
+	GolintInstallerURL = "github.com/golangci/golangci-lint/cmd/golangci-lint"
 )
