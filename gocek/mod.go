d9ae412c02a42115fc9db3ea825d293bc3524875
fahmi.irfan <fahmi.irfan@kumparan.com>
2020-04-10 11:03:47 +0700

    feature: add mod model

---
+++ gocek/mod.go
@@ -0,0 +1,25 @@
+package gocek
+
+import "time"
+
+// Module :nodoc:
+type Module struct {
+	Path    string    `json:"Path"`
+	Version string    `json:"Version"`
+	Time    time.Time `json:"Time"`
+	Update  struct {
+		Path    string    `json:"Path"`
+		Version string    `json:"Version"`
+		Time    time.Time `json:"Time"`
+	} `json:"Update"`
+	Indirect bool   `json:"Indirect"`
+	Dir      string `json:"Dir"`
+	GoMod    string `json:"GoMod"`
+}
+
+// SimpleModule :nodoc:
+type SimpleModule struct {
+	Path        string `json:"path"`
+	Version     string `json:"version"`
+	NextVersion string `json:"nextVersion"`
+}
