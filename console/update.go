09e74b40869862a3382be5abbc7e1b1f4faf18b7
Samuel Mulatua <samuel.jeremy@kumparan.com>
2020-04-13 15:29:27 +0700

    feature: add command to update fer to latest version

---
+++ console/update.go
@@ -0,0 +1,33 @@
+package console
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/kumparan/fer/installer"
+	"github.com/spf13/cobra"
+)
+
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "update fer",
+	Long:  `update fer to latest version`,
+	Run:   updateVersion,
+}
+
+func updateVersion(cmd *cobra.Command, args []string) {
+	updateCommand := exec.Command("go", "get", "-u", "github.com/kumparan/fer")
+	PrintInfo("Updating fer...")
+	err := updateCommand.Run()
+	if err != nil {
+		installer.ProgressBar(1)
+		fmt.Println("Failed updating fer")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	installer.ProgressBar(100)
+
+	PrintSuccess("Success updating fer!")
+}
