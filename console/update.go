a0b1ba80d901a0b20e10e7d1615a218ff26ef642
Agung Hariadi Tedja <agunghariaditedja@yahoo.com>
2020-04-14 17:11:53 +0700

    hotfix: fix fer update command

--- console/update.go
+++ console/update.go
@@ -17,7 +17,7 @@ var updateCmd = &cobra.Command{
 }
 
 func updateVersion(cmd *cobra.Command, args []string) {
-	updateCommand := exec.Command("go", "get", "-u", "github.com/kumparan/fer")
+	updateCommand := exec.Command("go", "get", "github.com/kumparan/fer@latest")
 	PrintInfo("Updating fer...")
 	err := updateCommand.Run()
 	if err != nil {
